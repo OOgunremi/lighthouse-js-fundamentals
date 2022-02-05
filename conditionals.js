db34997b7eedc12949383fdc82f1a7e160caa2f6
Olalekan Isaac Ogunremi <OOgunremi@yahoo.com>
2022-02-04 22:33:42 -0700

    conditionals tasks on compass prep work

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+
+const raining = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+const raining = true;
+const cold = false;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
+
+console.log("Now you're ready to go outside!");
+
+const cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
